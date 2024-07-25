6422098e513c286e13bb5c9db12d8474151f4895
Adam Bowman <adam.bowman@kainos.com>
2024-07-25 10:29:01 +0100

    finished all controller tests

--- src/main/controllers/AuthControllers.ts
+++ src/main/controllers/AuthControllers.ts
@@ -8,18 +8,10 @@ export const getLoginForm = async (req: express.Request, res: express.Response):
 
 export const postLoginForm  = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        const loginResquest4: LoginRequest = {
-            email: "adam@random.com",
-            password: "pass123"
-          }
-        console.log(loginResquest4);
         const loginRequest: LoginRequest = req.body;
-        console.log(req.body);
         req.session.token = await getTokenByloggingIn(loginRequest);
-        console.log(req.session.token);
         res.redirect('/logoutForm')
     } catch (e) {
-        console.log(e.message);
         res.locals.errormessage = e.message;
         res.render('pages/loginPage.html', req.body);
     }
--- src/test/unit/controllers/AuthContollerTests.ts
+++ src/test/unit/controllers/AuthContollerTests.ts
@@ -2,7 +2,7 @@ import * as AuthController from "../../../main/controllers/AuthControllers";
 import * as AuthService from "../../../main/services/AuthService";
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { Request, Response } from 'express'; 
+import { Request, Response } from 'express';
 import { LoginRequest } from "../../../main/models/LoginRequest";
 import { JwtToken } from "../../../main/models/JwtToken";
 import "express-session";
@@ -14,47 +14,128 @@ declare module "express-session" {
 }
 
 describe('AuthController', function () {
-    afterEach(() => {
-        sinon.restore();
-    });
-
-    describe('getLoginForm', function () {
-        it('should render login form view', async () => {
-          const req = { };
-          const res = { render: sinon.spy() };
-  
-          await AuthController.getLoginForm(req as any, res as any);
-  
-          expect(res.render.calledOnce).to.be.true;
-        });
-    });
-
-    describe('postLoginForm', function () {
-      it('should post login form and redirect to logout form view', async () => {
-
-        const loginRequestObj: LoginRequest = {
-          email: "adam@random.com",
-          password: "pass123"
-        }
-
-        sinon.stub(AuthService, 'getTokenByloggingIn').resolves('12345');
-
-        const req = {
-          body: loginRequestObj,
-          session: {token: '12345'}
-        };
-
-        const res = { 
-          render: sinon.spy(),
-          redirect: sinon.spy()
-        };
-
-        await AuthController.postLoginForm(req as any, res as any);
-
-        expect(res.redirect.calledOnce).to.be.true;
-        expect(res.redirect.calledWith('/logoutForm')).to.be.true;
-      });
+  afterEach(() => {
+    sinon.restore();
   });
 
+  describe('getLoginForm', function () {
+    it('should render login form view', async () => {
+      const req = {};
+      const res = { render: sinon.spy() };
+
+      await AuthController.getLoginForm(req as any, res as any);
+
+      expect(res.render.calledOnce).to.be.true;
+    });
+  });
+
+  describe('postLoginForm', function () {
+    it('should post login form and redirect to logout form view', async () => {
+
+      const loginRequestObj: LoginRequest = {
+        email: "adam@random.com",
+        password: "pass123"
+      }
+
+      sinon.stub(AuthService, 'getTokenByloggingIn').resolves('12345');
+
+      const req = {
+        body: loginRequestObj,
+        session: { token: '12345' }
+      };
+
+      const res = {
+        render: sinon.spy(),
+        redirect: sinon.spy()
+      };
+
+      await AuthController.postLoginForm(req as any, res as any);
+
+      expect(res.redirect.calledOnce).to.be.true;
+      expect(res.redirect.calledWith('/logoutForm')).to.be.true;
+    });
+
+    it('should render loginForm view with  User does not exist error message when said error is thrown', async () => {
+      const errorMessage: string = 'User does not exist.';
+      sinon.stub(AuthService, 'getTokenByloggingIn').resolves('12345').rejects(new Error(errorMessage));
+
+      const invalidEmailLoginRequestObj: LoginRequest = {
+        email: "invalid@random.com",
+        password: "pass123"
+      }
+
+      const req = {
+        body: invalidEmailLoginRequestObj,
+        session: { token: '12345' }
+      };
+
+      const res = {
+        render: sinon.spy(),
+        redirect: sinon.spy(),
+        locals: { errormessage: '' }
+      };
+
+      await AuthController.postLoginForm(req as any, res as any);
+
+      expect(res.render.calledOnce).to.be.true;
+      expect(res.locals.errormessage).to.equal(errorMessage);
+    });
+
+    it('should render loginForm view with  User is not valid: Invalid Credentials. when said error is thrown', async () => {
+      const errorMessage: string = 'User does not exist.';
+      sinon.stub(AuthService, 'getTokenByloggingIn').resolves('12345').rejects(new Error(errorMessage));
+
+      const invalidPasswordLoginRequestObj: LoginRequest = {
+        email: "adam@random.com",
+        password: "invalidpass"
+      }
+
+      const req = {
+        body: invalidPasswordLoginRequestObj,
+        session: { token: '12345' }
+      };
+
+      const res = {
+        render: sinon.spy(),
+        redirect: sinon.spy(),
+        locals: { errormessage: '' }
+      };
+
+      await AuthController.postLoginForm(req as any, res as any);
+
+      expect(res.render.calledOnce).to.be.true;
+      expect(res.locals.errormessage).to.equal(errorMessage);
+    });
+  });
+
+  describe('getLogoutForm', function () {
+    it('should render logout form view', async () => {
+      const req = {};
+      const res = { render: sinon.spy() };
+
+      await AuthController.getLogoutForm(req as any, res as any);
+
+      expect(res.render.calledOnce).to.be.true;
+    });
+  });
+
+  describe('postLogoutForm', function () {
+    it('should post logout form and redirect to login form view', async () => {
+
+      const req = {
+        session: { token: '12345' }
+      };
+
+      const res = {
+        render: sinon.spy(),
+        redirect: sinon.spy()
+      };
+
+      await AuthController.postLogoutForm(req as any, res as any);
+
+      expect(res.redirect.calledOnce).to.be.true;
+      expect(res.redirect.calledWith('/loginForm')).to.be.true;
+    });
+  });
 
 });
