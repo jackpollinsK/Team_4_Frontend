3677f267f07658bb6a11f60bef55f7b58320a23b
Adam Bowman <adam.bowman@kainos.com>
2024-07-29 14:41:27 +0100

    worked on auth

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import { getAllJobRoles } from "./main/controllers/JobRoleController";
 import { dateFilter } from "./main/filters/DateFilter";
 import { getLoginForm, getLogoutForm, postLoginForm, postLogoutForm } from "./main/controllers/AuthControllers";
 import { getHomePage } from "./main/controllers/HomeController";
+import { allowRoles } from "./main/middleware/AuthMiddleware";
+import { UserRole } from "./main/models/JwtToken";
 
 const app = express();
 
@@ -37,15 +39,13 @@ declare module "express-session" {
   }
 }
 
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
 app.get('/', getHomePage);
-app.get('/job-roles', getAllJobRoles);
-
-
 app.get('/loginForm', getLoginForm);
 app.post('/loginForm', postLoginForm);
 app.get('/logoutForm', getLogoutForm);
 app.post('/logoutForm', postLogoutForm);
-
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+app.get('/job-roles', allowRoles([UserRole.Admin, UserRole.User]), getAllJobRoles);
