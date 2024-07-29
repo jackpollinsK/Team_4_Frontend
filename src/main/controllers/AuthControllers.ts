33a29a37b31ea60c4fbbb960ebe7364cc7a5d7e3
Thomas Sargent <thomas.sargent@kainos.com>
2024-07-29 09:58:19 +0100

    Added hiding if user is logged in

--- src/main/controllers/AuthControllers.ts
+++ src/main/controllers/AuthControllers.ts
@@ -3,13 +3,13 @@ import { getTokenByloggingIn} from "../services/AuthService";
 import express from "express";
 
 export const getLoginForm = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('pages/loginPage.html', {pageName: 'Login page'});
+    res.render('pages/loginPage.html', {pageName: 'Login page', token: ''});
 }
 
 export const postLoginForm  = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
         const loginRequest: LoginRequest = req.body;
-        req.session.token = await getTokenByloggingIn(loginRequest);
+        const token = await getTokenByloggingIn(loginRequest);
         res.redirect('/')
     } catch (e) {
         res.locals.errormessage = e.message;
@@ -19,7 +19,7 @@ export const postLoginForm  = async (req: express.Request, res: express.Response
 }
 
 export const getLogoutForm = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('pages/logoutPage.html', {pageName: 'Logout page'});
+    res.render('pages/logoutPage.html', {pageName: 'Logout page', token: ''});
 }
 
 export const postLogoutForm = async(req: express.Request, res: express.Response): Promise<void> => {
--- src/main/controllers/JobRoleController.ts
+++ src/main/controllers/JobRoleController.ts
@@ -3,7 +3,7 @@ import { getJobRoles } from "../services/JobRoleService"
 
 export const getAllJobRoles = async (req: Express.Request, res: Express.Response): Promise<void> => {
     try{
-        res.render('pages/allJobRolesList.html', {jobRoles: await getJobRoles(), pageName: "Job Roles"});
+        res.render('pages/allJobRolesList.html', {jobRoles: await getJobRoles(), pageName: "Job Roles", token: ''});
     }catch (e) {
         res.locals.errormessage = e.message;
         res.locals.pageName = "Job Roles";
