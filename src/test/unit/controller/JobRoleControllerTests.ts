838103942f21c0b7b1e3d28b124919023fe22dde
Adam Bowman <adam.bowman@kainos.com>
2024-07-30 16:11:06 +0100

    finished authorisation

--- src/test/unit/controller/JobRoleControllerTests.ts
+++ src/test/unit/controller/JobRoleControllerTests.ts
@@ -4,6 +4,8 @@ import { JobRoleResponse } from "../../../main/models/JobRoleResponse";
 import { expect } from 'chai';
 import sinon from 'sinon';
 import express from "express";
+import { allowRoles } from "../../../main/middleware/AuthMiddleware";
+import { UserRole } from "../../../main/models/JwtToken";
 
 const expected: JobRoleResponse = {
     id: 1,
@@ -63,5 +65,32 @@ describe('JobRoleController', function () {
         expect(res.render.calledOnce).to.be.true;
         expect(res.render.calledWith('pages/allJobRolesList.html')).to.be.true;
         expect(res.locals.errormessage).to.equal(errorMessage);
+    });
+    it('should render not logged in view and return 401 status, when user is NOT logged in', async () => {
+ 
+        const req = {
+            session: { token: '' }
+        };
+ 
+        const res = {
+            render: sinon.spy(),
+            status: sinon.stub().returnsThis(),
+            redirect: sinon.stub().returnsThis()
+        };
+
+        const next = sinon.stub();
+ 
+        sinon.stub(JobRoleService, 'getJobRoles')
+ 
+        await JobRoleController.getAllJobRoles(req as express.Request, res as unknown as express.Response);
+
+        const middleware = allowRoles([UserRole.Admin, UserRole.User]);
+
+        await middleware(req as unknown as express.Request, res as unknown as express.Response, next);
+
+        expect((res.status as sinon.SinonStub).calledWith(401)).to.be.true;
+        expect(req.session.token).to.equal('');
+        expect(res.redirect.calledOnce).to.be.true;
+        expect(res.redirect.calledWith('/notLoggedIn')).to.be.true;
     });
 })
