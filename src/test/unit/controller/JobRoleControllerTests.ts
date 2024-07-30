3c646eca87d72bc5cfae9f21cdcbf0bbb5249fcb
Thomas Sargent <thomas.sargent@kainos.com>
2024-07-30 11:23:11 +0100

    Tests Writen for get by ID

--- src/test/unit/controller/JobRoleControllerTests.ts
+++ src/test/unit/controller/JobRoleControllerTests.ts
@@ -3,8 +3,9 @@ import * as JobRoleService from "../../../main/services/JobRoleService";
 import { JobRoleResponse } from "../../../main/models/JobRoleResponse";
 import { expect } from 'chai';
 import sinon from 'sinon';
+import { JobRoleSingleResponse } from "../../../main/models/JobRoleSingleResponse";
 
-const expected: JobRoleResponse = {
+const expectedJobRole: JobRoleResponse = {
     id: 1,
     roleName: "Manager",
     location: "New York", 
@@ -14,6 +15,19 @@ const expected: JobRoleResponse = {
     status : "open"
 }
 
+const expectedJobRoleSingle: JobRoleSingleResponse = {
+    id: 1,
+    roleName: "Engineer",
+    location: "Belfast",
+    capability: "Coding",
+    band: "A",
+    closingDate: new Date(2019, 1, 16),
+    status: "Open",
+    description: "A short description",
+    responsibilities: "Something Important",
+    jobSpec: "A Link to a page"
+}
+
 describe('JobRoleController',function() {
     afterEach(() => {
         sinon.restore();
@@ -21,7 +35,7 @@ describe('JobRoleController',function() {
 
     describe('getAllJobRoles', function () {
         it('should view Job Roles when Job Roles returned', async () => {
-            const jobRoleList = [expected];
+            const jobRoleList = [expectedJobRole];
 
             sinon.stub(JobRoleService, 'getJobRoles').resolves(jobRoleList);
             
@@ -47,4 +61,38 @@ describe('JobRoleController',function() {
         expect(res.render.calledWith('pages/allJobRolesList.html')).to.be.true;
         expect(res.locals.errormessage).to.equal(errorMessage);
       });
+
+
+describe('getJobRole', function () {
+    it('should view a Job Role when a Job Role is returned', async () => {
+
+        const job = expectedJobRoleSingle;
+
+        sinon.stub(JobRoleService, 'getJobRoleById').resolves(expectedJobRoleSingle);
+
+        const req = { params: {id: 1}};
+        const res = { render: sinon.spy() };
+
+        await JobRoleController.getJobRole(req as any, res as any);
+
+        expect(res.render.calledOnce).to.be.true;
+        expect(res.render.calledWith('pages/singleJobRole.html', {pageName: job.roleName+ ": " + job.band, job: job})).to.be.true;
+    });
+
+
+    it('should render view with error message when error thrown', async () => {
+        const errorMessage: string = 'Error message';
+        sinon.stub(JobRoleService, 'getJobRoleById').rejects(new Error(errorMessage));
+
+        const req = { params: {id: 1}};
+        const res = { render: sinon.spy(), locals: { errormessage: '' } };
+
+        await JobRoleController.getJobRole(req as any, res as any); // eslint-disable-line @typescript-eslint/no-explicit-any
+
+        expect(res.render.calledOnce).to.be.true;
+        expect(res.render.calledWith('pages/errorPage.html')).to.be.true;
+        expect(res.locals.errormessage).to.equal(errorMessage);
+      });
+
+})
 })
--- src/test/unit/services/JobRoleServicesTest.ts
+++ src/test/unit/services/JobRoleServicesTest.ts
@@ -3,50 +3,107 @@ import MockAdapter from "axios-mock-adapter";
 import { expect } from 'chai';
 import { JobRoleResponse } from "../../../main/models/JobRoleResponse";
 import { describe } from "node:test";
-import { getJobRoles, URL} from '../../../main/services/JobRoleService';
+import { getJobRoleById, getJobRoles, URL } from '../../../main/services/JobRoleService';
+import { JobRoleSingleResponse } from "../../../main/models/JobRoleSingleResponse";
 
 
 const expected: JobRoleResponse = {
-    id: 1,
-    roleName: "Manager",
-    location: "New York", 
-    capability: "Coding",
-    band: "B",
-    closingDate: new Date(2019, 1, 16),
-    status: "open"
+  id: 1,
+  roleName: "Manager",
+  location: "New York",
+  capability: "Coding",
+  band: "B",
+  closingDate: new Date(2019, 1, 16),
+  status: "open"
 }
+
+const expectedSingle: JobRoleSingleResponse = {
+  id: 1,
+  roleName: "Engineer",
+  location: "Belfast",
+  capability: "Coding",
+  band: "A",
+  closingDate: new Date(2019, 1, 16),
+  status: "Open",
+  description: "A short description",
+  responsibilities: "Something Important",
+  jobSpec: "A Link to a page"
+}
+
 const mock = new MockAdapter(axios);
 
 describe('JobRoleService', function () {
-    describe('getJobRoles', function () {
-        it('should return roles from response', async () => {
-            const data = [expected];
+  describe('getJobRoles', function () {
+    it('should return roles from response', async () => {
+      const data = [expected];
 
-            mock.onGet(URL).reply(200,data);
+      mock.onGet(URL).reply(200, data);
 
-            const results = await getJobRoles();
-  
-            expect(results[0].id).to.deep.equal(expected.id);
-            expect(results[0].roleName).to.deep.equal(expected.roleName);
-            expect(results[0].location).to.deep.equal(expected.location);
-            expect(results[0].capability).to.deep.equal(expected.capability);  
-            expect(results[0].band).to.deep.equal(expected.band); 
-            expect(results[0].closingDate).to.deep.equal(expected.closingDate.toISOString());
-            expect(results[0].status).to.deep.equal(expected.status);  
-        })
+      const results = await getJobRoles();
 
-    })
+      expect(results[0].id).to.deep.equal(expected.id);
+      expect(results[0].roleName).to.deep.equal(expected.roleName);
+      expect(results[0].location).to.deep.equal(expected.location);
+      expect(results[0].capability).to.deep.equal(expected.capability);
+      expect(results[0].band).to.deep.equal(expected.band);
+      expect(results[0].closingDate).to.deep.equal(expected.closingDate.toISOString());
+      expect(results[0].status).to.deep.equal(expected.status);
+    });
 
 
-it('should throw exception when 500 error returned from axios', async () => {
-    mock.onGet(URL).reply(500);
-    try {
-      await getJobRoles();
-    } catch (e) {
-      expect(e.message).to.equal('Failed to get JobRoles');
-      return;
-    }
-  })
-})
+    it('should throw exception when 500 error returned from axios', async () => {
+      mock.onGet(URL).reply(500);
+      try {
+        await getJobRoles();
+      } catch (e) {
+        expect(e.message).to.equal('Failed to get JobRoles');
+        return;
+      }
+    });
+    
+  });
+
+  describe('getJobRoleById', function () {
+    it('should return role from response', async () => {
+
+      mock.onGet(URL + "/1").reply(200, expectedSingle);
+
+      const results = await getJobRoleById("1");
+
+      expect(results.id).to.deep.equal(expectedSingle.id);
+      expect(results.roleName).to.deep.equal(expectedSingle.roleName);
+      expect(results.location).to.deep.equal(expectedSingle.location);
+      expect(results.capability).to.deep.equal(expectedSingle.capability);
+      expect(results.band).to.deep.equal(expectedSingle.band);
+      expect(results.closingDate).to.deep.equal(expectedSingle.closingDate.toISOString());
+      expect(results.status).to.deep.equal(expectedSingle.status);
+      expect(results.description).to.deep.equal(expectedSingle.description);
+      expect(results.responsibilities).to.deep.equal(expectedSingle.responsibilities);
+      expect(results.jobSpec).to.deep.equal(expectedSingle.jobSpec);
+
+    });
+
+    it('should throw exception when 404 error returned from axios', async () => {
+
+      mock.onGet(URL + "/1").reply(404, expectedSingle);
+      try {
+        await getJobRoleById("1");
+      } catch (e) {
+        expect(e.message).to.equal('Job Not Found');
+      }
+    });
+
+    it('should throw exception when 500 error returned from axios', async () => {
+
+      mock.onGet(URL + "/1").reply(500, expectedSingle);
+      try {
+        await getJobRoleById("1");
+      } catch (e) {
+        expect(e.message).to.equal('Sorry There is a problem on our end!');
+      }
+    });
+  });
+
+});
 
 
