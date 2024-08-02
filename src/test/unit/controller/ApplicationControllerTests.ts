f32232b11a1a582466d24aaef4a5c4faf1259270
Thomas Sargent <thomas.sargent@kainos.com>
2024-08-02 10:10:14 +0100

    Added snippets and get tests

--- src/test/unit/controller/ApplicationControllerTests.ts
+++ src/test/unit/controller/ApplicationControllerTests.ts
@@ -15,7 +15,7 @@ declare module 'express-session' {
   }
 }
 
-describe('AuthController', function () {
+describe('ApplicationController', function () {
   afterEach(() => {
     sinon.restore();
   });
@@ -36,64 +36,74 @@ describe('AuthController', function () {
         render: sinon.spy(),
       };
 
-      sinon.stub(ApplicationService, 'postJobRoleAplication');
       await ApplicationContoller.getApplyJobRolesForm(req as unknown as express.Request, res as unknown as express.Response);
 
       expect(res.render.calledOnce).to.be.true;
       expect(res.render.calledWith('pages/applyForJobRole.html', { id: req.params.id, pageName: 'Apply for a Job', token: req.session.token })).to.be.true;
     });
   });
-});
 
-/*
-it('should render Not logged in page when user is not signed in', async () => {
-  //Case for Successful Upload
+  it('should render Not logged in page when user is not signed in', async () => {
+    //Case for Successful Upload
+    const req = {
+      session: { token: '' },
+      params: { id: 1 }
+    };
 
-  const req = { session: {token: ''} };
-  const res = { render: sinon.spy() };
+    const res = {
+      render: sinon.spy(),
+      status: sinon.stub().returnsThis(),
+      redirect: sinon.stub().returnsThis(),
+      locals: { errormessage: '' }
+    };
 
-  await getApplyJobRolesForm(req as any, res as any);
-  
-  expect(res.render.calledOnce).to.be.true;
-  expect(res.render.calledWith('pages/notloggedIn.html', {pageName: 'Not logged in'})).to.be.true;
-});
+    const next = sinon.stub();
+
+    await ApplicationContoller.getApplyJobRolesForm(req as unknown as express.Request, res as unknown as express.Response);
+
+
+    const middleware = allowRoles([UserRole.Admin, UserRole.User]);
+
+    await middleware(req as unknown as express.Request, res as unknown as express.Response, next);
+
+    expect(res.render.calledOnce).to.be.true;
+    expect(res.redirect.calledWith('/notLoggedIn')).to.be.true;
+  });
+
+
+  describe('postApplicationForm', function () {
+    it('should render login form view', async () => {
+      //Case for Successful Upload
+    });
+
+    it('should render login form view', async () => {
+      //You must upload file
+    });
+
+    it('should render login form view', async () => {
+      //auth reqected
+    });
+
+    it('should render login form view', async () => {
+      //Case for Invalid format
+    });
+
+    it('should render login form view', async () => {
+      //Case for No file
+    });
+
+    it('should render login form view', async () => {
+      //Case for User already exists
+    });
+
+    it('should render login form view', async () => {
+      //Case for Aws error
+    });
+
+    it('should render login form view', async () => {
+      //Case for Server Error
+    });
+
+  });
 
 });
-
-describe('postApplicationForm', function () {
-it('should render login form view', async () => {
-  //Case for Successful Upload
-});
-
-it('should render login form view', async () => {
-  //You must upload file
-});
-
-it('should render login form view', async () => {
-  //auth reqected
-});
-
-it('should render login form view', async () => {
-  //Case for Invalid format
-});
-
-it('should render login form view', async () => {
-  //Case for No file
-});
-
-it('should render login form view', async () => {
-  //Case for User already exists
-});
-
-it('should render login form view', async () => {
-  //Case for Aws error
-});
-
-it('should render login form view', async () => {
-  //Case for Server Error
-});
-
-});
- 
-});
-*/
