361cc064f68ccfccad5ed2bebfabd1a4106e04c3
Henry Harkin <henry.harkin@kainos.com>
2024-08-05 16:41:48 +0100

    Created Job Role test object

---
+++ src/test/ui/JobRoleTestPage.ts
@@ -0,0 +1,25 @@
+import { WebDriver, WebElement } from 'selenium-webdriver';
+import { ChromeDriver } from './ChromeDriver';
+
+
+export class JobRoleTestPage extends ChromeDriver {
+
+    constructor(driver: WebDriver) {
+        super(driver);
+    }
+
+    async open(ROLE_NUMBER: number): Promise<void> {
+        const WEBSITE_URL = process.env.WEBSITE_URL;
+        const JOBROLE_URL = `${WEBSITE_URL}/job-roles-${ROLE_NUMBER}`;
+        await this.go_to_url(JOBROLE_URL);
+    }
+
+
+    //Element needs ID
+    async getRoleName(): Promise<string> {
+        const id = '/html/body/div/div/div/h1'; 
+        return await this.getTextByXpath(id); //Switch to ID when implemented
+    }
+
+
+}
