47357d1659d3b6d11cf535e4bdca254ce088aba6
Henry Harkin <henry.harkin@kainos.com>
2024-08-05 15:46:26 +0100

    Adding table functions to ChromeDriver

--- src/test/ui/ChromeDriver.ts
+++ src/test/ui/ChromeDriver.ts
@@ -1,5 +1,5 @@
 import chrome from 'selenium-webdriver/chrome';
-import { Builder, By, WebDriver } from 'selenium-webdriver';
+import { Builder, By, WebDriver, WebElement } from 'selenium-webdriver';
 
 export class ChromeDriver {
     driver: WebDriver;
@@ -41,6 +41,47 @@ export class ChromeDriver {
         console.log(`Getting text from element with LinkLext: ${linkText}`);
         return await this.driver.findElement(By.linkText(linkText)).getText();
     }
+
+    async getElementById(id: string) {
+        console.log(`Getting element with ID: ${id}`);
+        return await this.driver.findElement(By.id(id))
+    }
+
+    async getElementByXpath(path: string) {
+        console.log(`Getting element with XPath: ${path}`);
+        return await this.driver.findElement(By.xpath(path));
+    }
+
+    async getElementsByCSS(CSS: string) {
+        console.log(`Getting element with CSS: ${CSS}`);
+        return await this.driver.findElements(By.css(CSS));
+    }
+
+    async getTableById(id: string) { 
+        return await this.driver.findElement(By.id(id));
+    }
+
+    async getTableWithXpath(xpath: string): Promise<WebElement> {
+        return await this.driver.findElement(By.xpath(xpath));
+    }
+    
+    async getCellText(id: string, row: number, col: number): Promise<string> {
+        const table = await this.getTableWithXpath(id); //Switch to get with id
+        const cell = await table.findElement(By.css(`tbody tr:nth-child(${row}) td:nth-child(${col})`));
+        return cell.getText();
+    }
+
+    async getRowCount(id: string): Promise<number> {
+        const table = await this.getTableWithXpath(id); //Switch to get with id
+        const rows = await table.findElements(By.css('tbody tr'));
+        return rows.length;
+    }
+
+    async getColumnCount(id: string): Promise<number> {
+        const table = await this.getTableWithXpath(id); //Switch to get with id;
+        const columns = await table.findElements(By.css('thead th'));
+        return columns.length;
+    }    
 
     async clickById(id: string) {
         console.log(`Clicking element with ID: ${id}`);
