2009e17246ea0f5adcab79a3f9b57880b78d543d
Thomas Sargent <thomas.sargent@kainos.com>
2024-08-07 09:54:08 +0100

    Added tests for controller

--- src/main/controllers/OpenAIController.ts
+++ src/main/controllers/OpenAIController.ts
@@ -4,14 +4,19 @@ import { getQueryParams } from "../Utils/OpenAI";
 import { jwtDecode } from "jwt-decode";
 
 export const getPromptForm = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('pages/AIPrompt.html', {pageName: "AI Job finder", token: req.session.token});
+    res.render('pages/AIPrompt.html', {pageName: "AI Job finder", token: req.session.token, errormessage: res.locals.errorMessage});
 }
 
 export const postPromptForm = async (req: express.Request, res: express.Response): Promise<void> => {
 
+    try {
     const queryParams = await getQueryParams(req.body.prompt);
 
     const jobRolesResponse = await postAIResponse(queryParams, req.session.token);
 
     res.render("pages/allJobRolesList.html", {jobRoles: jobRolesResponse, pageName: "Job Roles", token: req.session.token, userLevel: jwtDecode(req.session.token)});
+    } catch (e) {
+        res.locals.errormessage = e.message;
+        res.redirect("/AI-Job-Search");
+    }
 }
--- src/test/unit/controller/OpenAIControllerTests.ts
+++ src/test/unit/controller/OpenAIControllerTests.ts
@@ -1,27 +1,159 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import express from "express";
+import { UserRole } from '../../../main/models/JwtToken';
+import jwt from 'jsonwebtoken';
+
+import { getPromptForm, postPromptForm } from '../../../main/controllers/OpenAIController';
+import * as OpenAIService from '../../../main/services/OpenAIService';
+import * as OpenAI from '../../../main/Utils/OpenAI';
+import { allowRoles } from '../../../main/middleware/AuthMiddleware';
+import { JobRoleResponse } from '../../../main/models/JobRoleResponse';
+import { jwtDecode } from 'jwt-decode';
+import { OpenAIRequest } from '../../../main/models/OpenAIRequest';
+
 
 describe('JobRoleController', function () {
     afterEach(() => {
         sinon.restore();
     });
 
-    describe('getPropmtForm', function () {
-        //Check if user is logged in
+    const secretKey = 'SUPER_SECRET';
+    const validJwtToken = jwt.sign({ Role: UserRole.User, sub: "test@random.com" }, secretKey, { expiresIn: '8h' });
 
-        //Check if user isn't logged in
+    const exampleJob: JobRoleResponse = {
+        id: 1,
+        roleName: 'Test Role',
+        location: 'Belfast',
+        capability: 'enginner',
+        band: 'senior',
+        closingDate: undefined,
+        status: 'open'
+    }
+
+    const aiQuerryResponse: OpenAIRequest = {
+        location: 'Belfast',
+        capability: 'enginner',
+        band: 'senior'
+    }
+
+    describe('getPropmtForm', function () {
+        it('Should render Prompt form when user is logged in', async () => {
+
+            const req = {
+                session: { token: validJwtToken }
+              };
+        
+              const res = {
+                render: sinon.spy(),
+              };
+
+            await getPromptForm(req as unknown as express.Request, res as unknown as express.Response);
+            expect(res.render.calledOnce).to.be.true;
+            expect(res.render.calledWith('pages/AIPrompt.html', {pageName: "AI Job finder", token: req.session.token})).to.be.true;
+
+        });
+
+        it('Should render Prompt form when user is not logged in', async () => {
+
+            const req = {
+                session: { token: '' }
+              };
+        
+              const res = {
+                render: sinon.spy(),
+                status: sinon.stub().returnsThis(),
+                redirect: sinon.stub().returnsThis(),
+                locals: { errormessage: '' }
+              };
+          
+              const next = sinon.stub();
+          
+
+            await getPromptForm(req as unknown as express.Request, res as unknown as express.Response);
+            const middleware = allowRoles([UserRole.Admin, UserRole.User]);
+
+            await middleware(req as unknown as express.Request, res as unknown as express.Response, next);
+        
+            expect(res.render.calledOnce).to.be.true;
+            expect(res.redirect.calledWith('/notLoggedIn')).to.be.true;
+
+        });
 
     });
 
     describe('postPropmtForm', function () {
-        //Check if post form is successful
+        //Check if post form is successful NOT WORKING
+        it.only('Should filter job roles when Open AI gives a successful', async () => {
+
+            const expectedList = [exampleJob]
+            
+            sinon.stub(OpenAI, 'getQueryParams').resolves(aiQuerryResponse)
+            sinon.stub(OpenAIService, 'postAIResponse').resolves(expectedList)
+
+
+            const req = {
+                session: { token: validJwtToken }
+              };
+        
+              const res = {
+                render: sinon.spy(),
+              };
+
+              await postPromptForm(aiQuerryResponse, req.session.token);
+
+              expect(res.render.calledOnce).to.be.true;
+              expect(res.render.calledWith("pages/allJobRolesList.html", {jobRoles: expectedList, pageName: "Job Roles", token: req.session.token, userLevel: jwtDecode(req.session.token)})).to.be.true;
+        });
 
         //Check if Open AI API returns an error
+        it.only('Should return an error message when OpenAIQuerry returns an error', async () => {
 
-        //Check if getAllJobs returns a 500
+            const expectedErrorMessage = 'There is an error with your request. Try again later';
+            sinon.stub(OpenAIService, 'postAIResponse').throws(new Error(expectedErrorMessage));
+            
+            const req = {
+                session: { token: validJwtToken }
+              };
 
-        //Check result when no results are found
+            const res = {
+                render: sinon.spy(),
+                status: sinon.stub().returnsThis(),
+                redirect: sinon.stub().returnsThis(),
+                locals: { errormessage: '' }
+              };
+              
+              try {
+                await postPromptForm(aiQuerryResponse, req.session.token);
+              } catch (e) {
+                expect(e.message).to.equal(expectedErrorMessage);
+              }
+        })
+
+        //Check if postAiResponse returns a 500
+        it.only('Should retunr an error message when getAllJobs returns a 500 status', async () => {
+
+            const expectedErrorMessage = 'Sorry There is a problem on our end!';
+            sinon.stub(OpenAIService, 'postAIResponse').throws(new Error(expectedErrorMessage));
+            
+            const req = {
+                session: { token: validJwtToken }
+              };
+
+            const res = {
+                render: sinon.spy(),
+                status: sinon.stub().returnsThis(),
+                redirect: sinon.stub().returnsThis(),
+                locals: { errormessage: '' }
+              };
+              
+              try {
+                await postPromptForm(aiQuerryResponse, req.session.token);
+              } catch (e) {
+                expect(e.message).to.equal(expectedErrorMessage);
+              }
+        })
+
 
     });
 });
