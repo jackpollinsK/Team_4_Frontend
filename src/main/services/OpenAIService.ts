ae51e0d4c67daee810bfc9eeacc50027e746887a
Thomas Sargent <thomas.sargent@kainos.com>
2024-08-08 10:27:37 +0100

    Ai search improvements

--- src/main/services/OpenAIService.ts
+++ src/main/services/OpenAIService.ts
@@ -1,38 +1,44 @@
+import { fileFromPath } from "openai";
 import { JobRoleResponse } from "../models/JobRoleResponse";
 import { OpenAIRequest } from "../models/OpenAIRequest";
 import { getJobRoles } from "./JobRoleService";
 
 
-export const postAIResponse = async (filter: OpenAIRequest, token: string): Promise<JobRoleResponse[]> => {
+export const postAIResponse = async (filterList: OpenAIRequest[], token: string): Promise<JobRoleResponse[]> => {
     try {
         const apiResponse = await getJobRoles(token);
-        return await aiFiltering(filter, apiResponse);
+        return await aiFiltering(filterList, apiResponse);
     } catch (e) {
-        if(e.message == "Failed to get JobRoles"){
+        if (e.message == "Failed to get JobRoles") {
             throw new Error("Sorry There is a problem on our end!");
         }
-        else{
+        else {
             throw new Error(e.message);
         }
     }
 }
 
-export const aiFiltering = async (filter: OpenAIRequest, jobRoles: JobRoleResponse[]): Promise<JobRoleResponse[]> => {
+export const aiFiltering = async (filterList: OpenAIRequest[], jobRoles: JobRoleResponse[]): Promise<JobRoleResponse[]> => {
 
     const result: JobRoleResponse[] = [];
+    try {
+        jobRoles.forEach(job => {
+            filterList.forEach(filter => {
+                if (filter.location.toLowerCase().includes(job.location.toLowerCase()) && filter.capability.toLowerCase().includes(job.capability.toLowerCase()) && filter.band.toLowerCase().includes(job.band.toLowerCase()))
+                    {
+                        result.push(job);
+                    }
+            })
+        })
 
-    jobRoles.forEach(job => {
-        if (filter.band.toLocaleLowerCase().includes(job.band.toLowerCase()) && 
-        filter.capability.toLowerCase().includes(job.capability.toLowerCase()) && 
-        filter.location.toLowerCase().includes(job.location.toLowerCase())){
+    } catch (e) {
+    throw new Error("Something went wrong please try again")
+}
 
-            result.push(job)
-        }
-    });
 
-    if (result.length == 0){
-        throw new Error("We couldn't find any jobs that would suit you")
-    }
+if (result.length == 0) {
+    throw new Error("We couldn't find any jobs that would suit you")
+}
 
-    return result;
+return result;
 }
